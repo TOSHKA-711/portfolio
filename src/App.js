53848688b79a24220d770053f6bef42b0cd837f7
TOSHKA-711 <aliovich711@gmail.com>
2025-02-07 15:06:40 +0200

    second

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-import "./App.css";
-import Navigation from "./component/navs/Navigation";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./component/navs/NavBar";
 import Home from "./component/Home";
 import About from "./component/About";
 import Services from "./component/Services";
 import Portfolio from "./component/Portfolio";
 import Contact from "./component/Contact";
-// import Footer from "./component/footer/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./component/navs/NavBar";
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/portfolio">
-        {/* <Navigation /> */}
-        <NavBar/>
+      <HashRouter>
+        <NavBar />
         <Routes>
-          <Route path="/" exact index element={<Home />} /> 
-          {/* <Route path="/" element={<Navigation />}> */}
-            <Route path="about" element={<About />} />
-            <Route path="services" element={<Services />} />
-            <Route path="portfolio" element={<Portfolio />} />
-            <Route path="contact" element={<Contact />} />
-          {/* </Route> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
